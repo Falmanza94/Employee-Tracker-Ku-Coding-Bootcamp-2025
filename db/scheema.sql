10494fe91b71e193c9d9eb6e2592ed44ec843f2d
Falmanza94 <fischer.almanza@gmail.com>
2025-03-01 16:38:56 -0600

    edits/revisions made in scheema.sql

--- db/scheema.sql
+++ db/scheema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
 
-/c staff_db;
+\c staff_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
@@ -11,8 +11,10 @@ CREATE TABLE department (
 CREATE TABLE role (
     id  SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL
-    department_id INTEGER NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,5 +22,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
