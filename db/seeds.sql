536b84c7bca952f16161a099ca3030f98024cdd8
Falmanza94 <fischer.almanza@gmail.com>
2025-03-01 18:10:40 -0600

    fixed syntax and deleted comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,18 +5,18 @@ VALUES  ('Curatorial'),
         ('Operations');
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Chief Curator', 120000, 1),  -- role_id = 1
-        ('Associate Curator', 85000, 1), -- role id =2
-        ('Curatorial Assistant', 45000, 1), -- role id =3
-        ('Marketing Officer',100000, 2), -- role id =4
-        ('Gifts and Donations', 75000, 2), -- role id =5
-        ('Corporate Partnerships',67000,2), -- role id =6
-        ('Public Programs', 115000, 3), -- role id =7
-        ('Curator of Education', 80000, 3), -- role id =8
-        ('School Programs', 58000, 3), -- role id =9
-        ('Facilities Manager', 105000, 4), -- role id =10
-        ('Museum Technology', 75000, 4), -- role id =11
-        ('Operations Coordinator', 58000, 4); -- role id =12
+VALUES  ('Chief Curator', 120000, 1),
+        ('Associate Curator', 85000, 1),
+        ('Curatorial Assistant', 45000, 1),
+        ('Marketing Officer', 100000, 2),
+        ('Gifts and Donations', 75000, 2),
+        ('Corporate Partnerships', 67000,2),
+        ('Public Programs', 115000, 3),
+        ('Curator of Education', 80000, 3),
+        ('School Programs', 58000, 3),
+        ('Facilities Manager', 105000, 4),
+        ('Museum Technology', 75000, 4),
+        ('Operations Coordinator', 58000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  
