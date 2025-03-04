bf86c6eeb719d9086c0f6c1632929e803d554b4c
Falmanza94 <fischer.almanza@gmail.com>
2025-03-04 15:07:58 -0600

    connection to staff db, and all values have been added as new cases with their values

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbName = process.env.DB_NAME;
 
 async function dbConnection(select) {
     try {
-        const db = await mysql.createConnection({
+        const db = await mysq1.createConnection({
             host: 'localhost',
             user: dbUser,
             password: dbPassword,
@@ -20,7 +20,41 @@ async function dbConnection(select) {
         let returnedOutputFromInq = [];
 
         switch (select) {
-            case 'View All Departments';
+            case 'View All Departments':
+                returnedRowsFromDb = await db.query('Select * From deparment');
+                console.table(returnedRowsFromDb[0]);
+                break;
+
+            case 'View All Roles':
+                returnedRowsFromDb = await db.query(`
+                    SELECT
+                        role.id,
+                        role.title,
+                        role.salary,
+                        department.name AS department
+                    FROM role
+                    JOIN department ON role.department_id = department.id
+                    `);
+                console.table(returnedRowsFromDb[0]);
+                break;
+
+            case 'View All Employess':
+                returnedRowsFromDb = await db.query(`
+                    SELECT
+                        employee.id,
+                        employee.first_name,
+                        employee.last_name,
+                        role.title AS title,
+                        department.name AS department,
+                        role.salary AS salary,
+                        CASE WHEN employee.manager_id IS NOT NULL THEN CONCAT(manager_table.first_name, ' ', manager_table.last_name) ELSE NULL END AS manager
+                    FROM employee
+                    JOIN role ON employee.role_id = role.id
+                    JOIN department ON role.department_id = department.id
+                    JOIN employee manager_table ON employee.manager_id = manager_table.id 
+                    `);
+                console.table(returnedRowsFromDb[0]);
+                break;
             
         }
     }
