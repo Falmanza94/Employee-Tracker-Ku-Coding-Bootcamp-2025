c960220e7dda6990003aec9e4e2303574a505bf3
Falmanza94 <fischer.almanza@gmail.com>
2025-03-06 14:37:55 -0600

    added user options

--- index.ts
+++ index.ts
@@ -22,27 +22,49 @@ const viewDepartments = async () => {
 
 //Function to display the prompts for the user
 const startApp = async () => {
-    const questions = [
+    const menuOptions = [
       {
         type: 'list',
         name: 'action',
         message: 'Welcome to the Museum Staff Database! How would you like to proceed?',
-        choices: [
-            'View all departments',
+        choices: 
+            ['View all departments',
             'View all roles',
             'View all employees',
             'Add a department',
             'Add a role',
             'Add an employee',
             'Update an employee role',
-            'End Application'
-        ]
+            'End Application']
       }
     ];
 
+const { action } = await inquirer.prompt(menuOptions);
 
-
-
-
-
-}
+switch (action) {
+    case 'View all departments':
+      await viewDepartments();
+      break
+    case 'View all roles':
+      await viewRoles();
+      break
+    case 'View all employees':
+      await viewEmployees();
+      break
+    case 'Add a department':
+      await addDepartment();
+      break
+    case 'Add a role':
+      await addRole();
+      break
+    case 'Add an employee':
+      await addEmployee();
+      break
+    case 'Update an employee role':
+      await updateEmployeeRole();
+      break
+    case 'End Application':
+      console.log('Ending...');
+      process.exit();
+    }
+};
